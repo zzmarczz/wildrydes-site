5fa764e2d5d066e83a01ffa3432fc49e7262d744
Marcos Sanchez <sanchez.mrcs@gmail.com>
2024-01-30 21:22:18 -0600

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_OIMedYsCZ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7tuv8u05icju1vrlejuiechi6q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-west-1_GHtxgkXbL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '53lgtatsrrg95s4dtj1ibld5jt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://yvyefzepgc.execute-api.us-east-1.amazonaws.com/prod',
